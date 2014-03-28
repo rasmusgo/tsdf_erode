3cbf37d0493999c67de412ccdc245bbca5d3846e
Rasmus Göransson <rasmusgo@kth.se>
2014-03-28 18:50:01 +0100

    Generate tsdf plane without pixelated image plane

--- tsdf_plane.m
+++ tsdf_plane.m
@@ -1,23 +1,25 @@
 function [ Z W ] = tsdf_plane( N, M, trunc_dist, ang, w, smoothW )
 %TSDF_PLANE Simulate 1D depth images of a plane from arbitrary angle
-[X Y] = meshgrid(M, M);
-k = tan(ang); m = 0;
+
+%% Create rotated coordinate system
+[X, Y] = meshgrid(M, M);
 R = [cos(ang) -sin(ang);sin(ang) cos(ang)];
 X2 = R(1,1) * X + R(1,2) * Y;
 Y2 = R(2,1) * X + R(2,2) * Y;
 
-x = M;
-y = k*x + m;
-y(abs(x) > w*cos(ang)) = 100;
+%% Raytrace in eye-space
+k = tan(ang); m = 0;
 Z = zeros(N,N) - 100;
 for ix = 1:N
     for iy = 1:N
-        ii = round(50+5*X2(iy,ix));
-        if ii > 0 && ii <= N
-            Z(iy,ix) = y(ii)-Y2(iy,ix);
+        if abs(X2(iy,ix)) > w*cos(ang)
+            Z(iy,ix) = trunc_dist;
+        else
+            Z(iy,ix) = k*X2(iy,ix) + m - Y2(iy,ix);
         end
     end
 end
+
 W = double(Z >= -trunc_dist);
 if smoothW
     Idx = (Z >= -trunc_dist) & (Z < 0);
@@ -28,4 +30,3 @@ end
 Z(Z>trunc_dist) = trunc_dist;
 Z(Z<-trunc_dist) = 0;
 end
-
