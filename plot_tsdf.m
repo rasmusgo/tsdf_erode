29d2be6457c384fec2b7766107941d649f889c82
Rasmus Göransson <rasmusgo@kth.se>
2014-03-28 18:13:36 +0100

    plot tsdf weights in subplot

--- plot_tsdf.m
+++ plot_tsdf.m
@@ -1,15 +1,34 @@
 function plot_tsdf( Z, W, trunc_dist )
 %PLOT_TSDF Show TSDF values and plot contour
     clf;
+
+    % avoid plotting next to zero weight
+    Z(W==0) = NaN;
+
+    % plot distance
+    subplot(1,2,1)
     hold on
     imagesc(Z, [-trunc_dist, trunc_dist])
     colormap(hot(1024))
     colorbar
-    Z(W==0) = NaN;
     contour(Z, [-trunc_dist trunc_dist], '-g')
     contour(Z, [0 0], '-b', 'LineWidth', 2)
     %colormap hot
     axis equal xy tight
     hold off
+
+    % plot weights
+    subplot(1,2,2)
+    hold on
+    imagesc(W, [0,max(1, max(W(:)))])
+    contour(Z, [-trunc_dist trunc_dist], '-g')
+    contour(Z, [0 0], '-b', 'LineWidth', 2)
+    %colormap(gray)
+    colorbar
+    axis equal xy tight
+    hold off
+
+    % set first subplot as active
+    subplot(1,2,1)
 end
 
