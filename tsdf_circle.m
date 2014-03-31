23b150b1e50434d9482f0461df3923578421d9de
Rasmus Göransson <rasmusgo@kth.se>
2014-03-31 14:47:08 +0200

    Support for multiple viewing angles in tsdf_polygon and tsdf_circle

--- tsdf_circle.m
+++ tsdf_circle.m
@@ -1,14 +1,34 @@
 function [ Z W ] = tsdf_circle( N, M, trunc_dist, ang, x, y, r, smoothW )
 %TSDF_CIRCLE Simulate 1D depth images of a circle from arbitrary angle
+
+%% Call self repeatedly if multiple angles are given
+if numel(ang) > 1
+    Z = zeros(N);
+    W = zeros(N);
+    for a = ang
+        [Za, Wa] = tsdf_circle(N, M, trunc_dist, a, x, y, r, smoothW);
+        Z = Z + Za .* Wa;
+        W = W + Wa;
+    end
+
+    Z = Z ./ W;
+    Z(W==0) = 0;
+    return
+end
+
+%% Create coordinate system with offset
 [X, Y] = meshgrid(M-x, M-y);
 R = [cos(ang) -sin(ang);sin(ang) cos(ang)];
 X2 = R(1,1) * X + R(1,2) * Y;
 Y2 = R(2,1) * X + R(2,2) * Y;
 
+%% Raytrace in eye-space
+% Compute projective distance
 Z = -sqrt(r^2 - X2.^2) - Y2;
 Z(imag(Z)~=0) = 100;
 Z = real(Z);
 
+% Compute weights
 if smoothW
     W = 1 + Z / trunc_dist;
     W(Z >= 0) = 1;
@@ -17,6 +37,7 @@ else
     W = double(Z >= -trunc_dist);
 end
 
+% Truncate distances
 Z(Z > trunc_dist) = trunc_dist;
 Z(Z < -trunc_dist) = 0;
 end
--- tsdf_polygon.m
+++ tsdf_polygon.m
@@ -3,6 +3,21 @@ function [ Z W ] = tsdf_polygon( N, M, trunc_dist, ang, P, smoothW )
 %   [ Z W ] = tsdf_polygon( N, M, trunc_dist, ang, P, smoothW )
 % P should be a 2 x n matrix, where n is the number of vertices 
 
+%% Call self repeatedly if multiple angles are given
+if numel(ang) > 1
+    Z = zeros(N);
+    W = zeros(N);
+    for a = ang
+        [Za, Wa] = tsdf_polygon(N, M, trunc_dist, a, P, smoothW);
+        Z = Z + Za .* Wa;
+        W = W + Wa;
+    end
+
+    Z = Z ./ W;
+    Z(W==0) = 0;
+    return
+end
+
 %% Create rotated coordinate system
 [X, Y] = meshgrid(M, M);
 R = [cos(ang) -sin(ang);sin(ang) cos(ang)];
