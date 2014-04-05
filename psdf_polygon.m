fedb2213952b8e8ccb1be2baf14de24bdabd543e
Rasmus Göransson <rasmusgo@kth.se>
2014-04-05 13:28:29 +0200

    Resolution multiplier for more precise bwdist estimates

--- psdf_polygon.m
+++ psdf_polygon.m
@@ -1,10 +1,13 @@
-function prob = psdf_polygon( N, M, prob_range, ang, P )
+function prob = psdf_polygon( N, M, prob_range, ang, P, multiplier )
+if nargin < 6
+    multiplier = 1;
+end
 %% Call self repeatedly if multiple angles are given
 if numel(ang) > 1
-    prob = psdf_polygon( N, M, prob_range, ang(1), P );
+    prob = psdf_polygon( N, M, prob_range, ang(1), P, multiplier );
     for a = ang(2:end)
         % call self
-        p = psdf_polygon( N, M, prob_range, a, P );
+        p = psdf_polygon( N, M, prob_range, a, P, multiplier );
         prob = prob .* p;
     end
     return
@@ -15,8 +18,10 @@ L = visible_lines(ang, P);
 
 %% Open hypothesis
 % Draw the outline of the polygon and make a distance transform
-img = zeros(N);
-scale = 1 / (M(2)-M(1));
+%multiplier = 4;
+N2 = N*multiplier;
+img = zeros(N2);
+scale = multiplier / (M(2)-M(1));
 L2 = round((L-M(1))*scale + 1);
 for i = 1:size(L2,2)
     img = draw_line(img, L2(1:2,i), L2(3:4,i), 1);
@@ -34,8 +39,8 @@ Zopen = bwdist(img) / scale;
 
 % Connect the lines and extend edges away from camera
 R = [cos(ang) -sin(ang);sin(ang) cos(ang)];
-p1 = L2(1:2, 1)   + round(1.5*N * R(2,:)');
-p2 = L2(3:4, end) + round(1.5*N * R(2,:)');
+p1 = L2(1:2, 1)   + round(1.5*N2 * R(2,:)');
+p2 = L2(3:4, end) + round(1.5*N2 * R(2,:)');
 shape = [p1(:); L2(:); p2];
 P3 = reshape(shape, 2, numel(shape)/2);
 
@@ -54,21 +59,25 @@ img = img(:,:,1);
 Zclosed = bwdist(img) / scale;
 
 % Set the inside to negative distances
-mask = poly2mask(P3(1,:), P3(2,:), N, N);
+mask = poly2mask(P3(1,:), P3(2,:), N2, N2);
 Zclosed(mask) = -Zclosed(mask);
 
-% imagesc(Zclosed, [-trunc_dist, trunc_dist])
+% imagesc(Zclosed, [prob_range(1), prob_range(end)])
 % axis xy equal tight
 
+% Undo scale multiplier
+Zopen = Zopen(1:multiplier:end, 1:multiplier:end);
+Zclosed = Zclosed(1:multiplier:end, 1:multiplier:end);
+
 %% Convert range of possible values to probability distribution
-epsilon = 0.5/scale;
+epsilon = 0.5 * (M(2)-M(1));
 prob_range_mid = prob_range(1:end-1) + diff(prob_range)*0.5;
 prob_range_lo = [-inf, prob_range_mid] - epsilon;
 prob_range_hi = [prob_range_mid, inf] + epsilon;
 Np = numel(prob_range);
 prob = zeros(N,N,Np);
 for i = 1:Np
-    prob(:,:,i) = double(Zopen > prob_range_lo(i) & Zclosed < prob_range_hi(i));
+    prob(:,:,i) = double(Zopen >= prob_range_lo(i) & Zclosed <= prob_range_hi(i));
 end
 
 %plot_probability(prob)
