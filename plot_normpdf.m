fd300083bb79a5f85151fda9e3d9eabb3f7aeb4a
Rasmus Göransson <rasmusgo@kth.se>
2014-04-06 13:06:33 +0200

    Use cdf = 0.5 as a better way of estimating expected value

--- plot_normpdf.m
+++ plot_normpdf.m
@@ -15,6 +15,31 @@ plot(mu, 0, 'ob', 'LineWidth', 2, 'MarkerSize', 10);
 Z = prob_range(:)' * prob(:) / sum(prob(:));
 plot(Z, 0, 'xr', 'LineWidth', 2, 'MarkerSize', 10);
 
+p = cumsum(prob(:));
+i = find(p >= 0.5, 1, 'first');
+if i > 1 && i < numel(p)
+    y1 = p(i-1);
+    y2 = p(i);
+    x1 = prob_range_limits(i-1);
+    x2 = prob_range_limits(i);
+    plot(prob_range_limits, p(1:end-1), '-y')
+    plot([x1 x2], [y1 y2], '-g', 'LineWidth', 3)
+%     y = kx + m
+%     k = (y2-y1) / (x2-x1);
+%     m = y1 -k*x1;
+%     x = (y - m)/k;
+%     x = (y - y1 + k*x1)/k;
+%     x = (y - y1)/k + x1;
+%     x = (y - y1)*(x2-x1)/(y2-y1) + x1;
+%     y = 0.5
+%     x = (0.5 - y1)*(x2-x1)/(y2-y1) + x1;
+    Z2 = (0.5 - y1)*(x2-x1)/(y2-y1) + x1;
+else
+    Z2 = prob_range(i);
+end
+
+plot(Z2, 0, '+g', 'LineWidth', 2, 'MarkerSize', 10);
+
 %% Plot normal distribution on top
 X = linspace(xlims(1), xlims(2), 300);
 plot(X, normpdf(X, mu, sigma), 'LineWidth', 2);
